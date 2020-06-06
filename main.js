b76ef5433e1983fe54892390301403d00dacb0cf
Ryuma <lymstol@gmail.com>
2020-06-06 21:07:38 +0900

    円の描画確認完了

--- main.js
+++ main.js
@@ -2,18 +2,14 @@ var map;
 var placeList = [];
 var currentPositionMarker;
 var searchedPlaceMarkers = [];
+var searchedPlaceCircles = [];
 var language = navigator.language;
-
+var radius;
 document.addEventListener("DOMContentLoaded", function(){
     //document.getElementById("search").onclick = buttonClickHandler;
     initMap([38.4874154,140.375671]);
 }
 )
-
-//function init() {
-//    document.getElementById("search").onclick = buttonClickHandler;
-//    initMap([38.4874154,140.375671]);
-//}
 
 function initMap(latLng) {
     var latLng = {lat: latLng[0], lng: latLng[1]};
@@ -26,8 +22,16 @@ function initMap(latLng) {
 function buttonClickHandler() {
     //エラーハンドリングも実装する。
     placeList = [];
+    // 表示しているマーカーを消す
+    for(var i = 0; i<searchedPlaceMarkers.length; i++)
+    {
+        searchedPlaceMarkers[i].setMap(null);
+    }
+    searchedPlaceMarkers = [];
+   
     var latLngBounds = map.getBounds();
     var keyword = document.getElementById("placeToSearch").value;
+        radius = document.getElementById("raduisToDraw").value * 1000; //ToDo km,m変換に対応する
     searchPlaces(latLngBounds,keyword)
 }
 
@@ -50,12 +54,12 @@ function callback(results, status, pagination)
         for(var i=0; i<results.length; i++)
         {
             var info = {
-                name: results[i].name,                    // 店名
-                location: results[i].geometry.location,   // 座標
+                name: results[i].name,                          // 店名
+                location: results[i].geometry.location,         // 座標
                 formatted_address: results[i].formatted_address,//可読な住所
-                vicinity: results[i].vicinity,             // 住所(simplified address for the place)
+                vicinity: results[i].vicinity,                  // 住所(simplified address for the place)
                 address_component: results[i].address_component,  // 住所の構成要素の配列(?)
-                adr_address: results[i].adr_address,       //短縮した住所(?)
+                adr_address: results[i].adr_address,            //短縮した住所(?)
             };
             placeList.push(info);
         }
@@ -66,6 +70,7 @@ function callback(results, status, pagination)
         else
         {
             //OutputResults();
+            initListOfTasks();
         }
     }
     else
@@ -184,4 +189,73 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos)
                           'Error: The Geolocation service failed.' :
                           'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
-  }
+}
+
+
+//検索結果よりカードを生成
+let cardContainer;
+
+let createTaskCard = (element) => {
+
+let card = document.createElement('div');
+card.className = 'card shadow cursor-pointer';
+
+let cardBody = document.createElement('div');
+cardBody.className = 'card-body';
+
+let title = document.createElement('h5');
+title.innerText = element.name;
+title.className = 'card-title';
+
+let color = document.createElement('div');
+color.innerText = element.formatted_address;
+color.className = 'card-color';
+
+cardBody.appendChild(title);
+cardBody.appendChild(color);
+card.appendChild(cardBody);
+cardContainer.appendChild(card);
+
+}
+
+let addMarkerPlaces = (element) => {
+    var marker = new MarkerWithLabel({
+        map: map,
+        position: element.location,
+        icon: {
+            url:'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            scaledSize : new google.maps.Size(32, 32)
+        },
+        labelContent: element.name,                    //ラベル文字
+        labelAnchor: new google.maps.Point(-10, 25),   //ラベル文字の基点
+        labelClass: 'mapLabels',                       //CSSのクラス名
+        labelStyle: {opacity: 0.8},                    //透過度
+        animation: google.maps.Animation.DROP,
+    });
+    searchedPlaceMarkers.push(marker)
+}
+
+let addCercleAroundPlaces = (element) => {
+    var circle = new google.maps.Circle({
+        map: map,
+        center: element.location,
+        radius : radius
+    });
+    searchedPlaceCircles.push(circle)
+}
+
+
+
+let initListOfTasks = () => {
+if (cardContainer) {
+    document.getElementById('card-container').replaceWith(cardContainer);
+    return;
+}
+
+cardContainer = document.getElementById('card-container');
+placeList.forEach((element) => {
+    createTaskCard(element);
+    addMarkerPlaces(element);
+    addCercleAroundPlaces(element);
+});
+};
